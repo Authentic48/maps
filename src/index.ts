8808fc84fcf44b88e7482c8eb04d5cb66e8d57d4
Fall Jr <falljr@Falls-MBP.sibttk.net>
2021-08-19 16:20:29 +0700

    Custom maps created

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 // import { User } from './User';
 // import { Company } from './Company';
 
+import { CustomMap } from "./CustomMap";
 // const user = new User();
 
 // const company = new Company();
@@ -8,10 +9,4 @@
 // console.log(company);
 // console.log(user);
 
-new google.maps.Map(document.getElementById('map'), {
-  zoom: 1,
-  center: {
-    lat: 0,
-    lng: 0,
-  },
-});
+new CustomMap('map')
